3b5a71371d4bacd6f7e456eb52dec5391d14e4b4
KanoidSan <kanoid1996@op.pl>
2018-07-05 16:14:18 +0200

    ended 20'th episode of spring course by springGuru

--- src/main/java/com/kanoidsan/Learning/try04/scaningPlace/controllers/GreetingController.java
+++ src/main/java/com/kanoidsan/Learning/try04/scaningPlace/controllers/GreetingController.java
@@ -20,7 +20,7 @@ public class GreetingController {
     }
 
     @Autowired
-    @Qualifier("helloWorldServicePolish")
+    @Qualifier("hellorldServicePolish")
     public void setHelloWorldServicePolish(HelloWorldService helloWorldService) {
         this.helloWorldServicePolish = helloWorldService;
     }
