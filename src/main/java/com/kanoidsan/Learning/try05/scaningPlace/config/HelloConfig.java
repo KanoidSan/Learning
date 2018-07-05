0ffecce7c874e20284ec1b6d018ce088d9a9960b
KanoidSan <kanoid1996@op.pl>
2018-07-05 17:28:02 +0200

    spring by springGuru

--- src/main/java/com/kanoidsan/Learning/try05/scaningPlace/config/HelloConfig.java
+++ src/main/java/com/kanoidsan/Learning/try05/scaningPlace/config/HelloConfig.java
@@ -1,43 +1,36 @@
 package com.kanoidsan.Learning.try05.scaningPlace.config;
 
-import com.kanoidsan.Learning.try05.scaningPlace.services.HelloWorldFactory;
-import com.kanoidsan.Learning.try05.scaningPlace.services.HelloWorldService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
+//@Configuration
 public class HelloConfig {
 
-    @Bean
-    public HelloWorldFactory helloWorldFactory(){
-        return new HelloWorldFactory();
-    }
+//    public HelloWorldFactory helloWorldFactory(){
+//        return new HelloWorldFactory();
+//    }
 
-    @Bean
-    @Profile("english")
-    public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory){
-        return factory.createHelloWorld("en");
-    }
+//    @Bean
+//    @Profile("english")
+//    public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory){
+//        return factory.createHelloWorld("en");
+//    }
 
-    @Bean
-    @Profile("spanish")
-    public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory){
-        return factory.createHelloWorld("es");
-    }
+//    @Bean
+//    @Profile("spanish")
+//    public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory){
+//        return factory.createHelloWorld("es");
+//    }
 
-    @Bean
-    public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory){
-        return factory.createHelloWorld("pl");
-    }
+//    @Bean
+//    public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory){
+//        return factory.createHelloWorld("pl");
+//    }
 
-    @Bean
-    public HelloWorldService helloWorldServiceDeutsch(HelloWorldFactory factory){
-        return factory.createHelloWorld("de");
-    }
+//    @Bean
+//    public HelloWorldService helloWorldServiceDeutsch(HelloWorldFactory factory){
+//        return factory.createHelloWorld("de");
+//    }
 
-    @Bean(name = "french")
-    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory){
-        return factory.createHelloWorld("fr");
-    }
+//    @Bean(name = "french")
+//    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory){
+//        return factory.createHelloWorld("fr");
+//    }
 }
--- src/main/java/com/kanoidsan/Learning/try05/scaningPlace/controllers/GreetingController.java
+++ src/main/java/com/kanoidsan/Learning/try05/scaningPlace/controllers/GreetingController.java
@@ -1,11 +1,7 @@
 package com.kanoidsan.Learning.try05.scaningPlace.controllers;
 
 import com.kanoidsan.Learning.try05.scaningPlace.services.HelloWorldService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
-@Component
 public class GreetingController {
     private HelloWorldService helloWorldService;
     private HelloWorldService helloWorldServicePolish;
@@ -13,20 +9,15 @@ public class GreetingController {
 
     //to every implementation you need to add qualifier, if you don't do that,
     // Spring will not recognize which instance should he pass
-    @Autowired
-    @Qualifier("helloWorldServiceEnglish")
+
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    @Autowired
-    @Qualifier("helloWorldServicePolish")
     public void setHelloWorldServicePolish(HelloWorldService helloWorldService) {
         this.helloWorldServicePolish = helloWorldService;
     }
 
-    @Autowired
-    @Qualifier("french")
     public void setHelloWorldServiceFrench(HelloWorldService helloWorldService) {
         this.helloWorldServiceFrench = helloWorldService;
     }
