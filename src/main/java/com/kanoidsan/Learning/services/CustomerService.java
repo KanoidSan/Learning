bcaf4d843ab861e64ef69af16ba0705ea850ce51
KanoidSan <kanoid1996@op.pl>
2018-07-12 13:09:29 +0200

    setup to begin mvc spring course by spring guru intoduced to tests

---
+++ src/main/java/com/kanoidsan/Learning/services/CustomerService.java
@@ -0,0 +1,10 @@
+package com.kanoidsan.Learning.services;
+
+import com.kanoidsan.Learning.domain.Customer;
+
+/**
+ * Created by jt on 11/14/15.
+ */
+public interface CustomerService extends CRUDService<Customer>{
+
+}
